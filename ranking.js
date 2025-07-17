78e4871de3fb65ef2b7fa9408b5b7f383aa7db58
andrearcalmet09 <andrearcalmet09@gmail.com>
2025-07-17 15:41:58 -0500

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,1 +1,25 @@
+function calcularRankingFinal() {
+  const promedioEl = document.querySelector("#promedio-general strong");
+  const rankingEl = document.querySelector("#ranking strong");
+
+  const ppa = parseFloat(promedioEl.textContent);
+  if (isNaN(ppa)) {
+    rankingEl.textContent = "-";
+    return;
+  }
+
+  let ranking = "📄 Regular";
+
+  if (ppa >= 18) {
+    ranking = "🏅 Décimo Superior";
+  } else if (ppa >= 17) {
+    ranking = "🥈 Quinto Superior";
+  } else if (ppa >= 16) {
+    ranking = "🎖️ Tercio Superior";
+  } else if (ppa >= 14.5) {
+    ranking = "📘 Medio Superior";
+  }
+
+  rankingEl.textContent = ranking;
+}
 
